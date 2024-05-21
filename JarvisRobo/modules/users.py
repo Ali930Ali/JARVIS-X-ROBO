51cf730245c1e46cc0b389b57658a8186d4848bd
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2024-05-21 12:24:50 +0300

    Update users.py

--- JarvisRobo/modules/users.py
+++ JarvisRobo/modules/users.py
@@ -256,5 +256,5 @@ dispatcher.add_handler(USER_HANDLER, USERS_GROUP)
 dispatcher.add_handler(CHATLIST_HANDLER)
 dispatcher.add_handler(CHAT_CHECKER_HANDLER, CHAT_GROUP)
 
-__mod_name__ = "Usᴇʀs"
+__mod_name__ = "✨Usᴇʀs✨"
 __handlers__ = [(USER_HANDLER, USERS_GROUP), CHATLIST_HANDLER]
