dda53e0efa61c85f6567294865f6b2d92f1ff5ad
Ali930Ali <alibeff48@gmail.com>
2024-06-06 14:08:22 +0400

    Update __init__.py

--- JarvisRobo/__init__.py
+++ JarvisRobo/__init__.py
@@ -7,9 +7,9 @@ from aiohttp import ClientSession
 from pyrogram import Client
 from telethon import TelegramClient
 
-StartTime = time.time()
+BaşlangıçZamanı = time.time()
 
-# enable logging
+# günlüğü etkinleştir
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
@@ -21,69 +21,69 @@ logging.getLogger("telethon").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
-# if version < 3.6, stop bot.
+# eğer versiyon < 3.6 ise, botu durdur.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     LOGGER.error(
-        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
+        "En az 3.6 python sürümüne sahip OLMALISINIZ! Birçok özellik buna bağlıdır. Bot çıkıyor."
     )
     quit(1)
 
-ENV = bool(os.environ.get("ENV", False))
+ENV = bool(os.environ.get("ÇEVRE", False))
 
 if ENV:
 
-    API_ID = int(os.environ.get("API_ID", None))
+    API_ID = int(os.environ.get("API_KİMLİK", None))
     API_HASH = os.environ.get("API_HASH", None)
     
-    ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
-    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
-    CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
-    DB_URI = os.environ.get("DATABASE_URL")
-    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
-    EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
-    INFOPIC = bool(os.environ.get("INFOPIC", "True"))
-    LOAD = os.environ.get("LOAD", "").split()
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
-    NO_LOAD = os.environ.get("NO_LOAD", "").split()
-    START_IMG = os.environ.get("START_IMG", "")
-    HELP_IMG = os.environ.get("HELP_IMG", "https://telegra.ph/file/61296490da95c55a1d5ee.jpg")
-    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Dora_Hub")
-    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
-    TOKEN = os.environ.get("TOKEN", None)
-    TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
-    WORKERS = int(os.environ.get("WORKERS", 8))
+    ALLOW_CHATS = os.environ.get("CHATS_İZİN", True)
+    ALLOW_EXCL = os.environ.get("EXCL_İZİN", False)
+    CASH_API_KEY = os.environ.get("PARA_API_ANAHTAR", None)
+    DB_URI = os.environ.get("VERİTABANI_URL")
+    DEL_CMDS = bool(os.environ.get("KOMUTLARI_SİL", False))
+    EVENT_LOGS = os.environ.get("OLAY_GÜNLÜKLERİ", None)
+    INFOPIC = bool(os.environ.get("BİLGİ_RESMİ", "True"))
+    LOAD = os.environ.get("YÜKLE", "").split()
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URL", None)
+    NO_LOAD = os.environ.get("YÜKLEME", "").split()
+    START_IMG = os.environ.get("BAŞLANGIÇ_RESMİ", "")
+    HELP_IMG = os.environ.get("YARDIM_RESMİ", "https://telegra.ph/file/61296490da95c55a1d5ee.jpg")
+    STRICT_GBAN = bool(os.environ.get("KATI_YASAK", True))
+    SUPPORT_CHAT = os.environ.get("DESTEK_SOHBETİ", "Dora_Hub")
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("GEÇİCİ İNDİRME_DİZİNİ", "./")
+    TOKEN = os.environ.get("ANAHTAR", None)
+    TIME_API_KEY = os.environ.get("ZAMAN_API_ANAHTAR", None)
+    WORKERS = int(os.environ.get("ÇALIŞANLAR", 8))
 
     try:
-        OWNER_ID = int(os.environ.get("OWNER_ID", None))
+        OWNER_ID = int(os.environ.get("SAHİP_KİMLİĞİ", None))
     except ValueError:
-        raise Exception("Your OWNER_ID env variable is not a valid integer.")
+        raise Exception("SAHİP_KİMLİĞİ env değişkeniniz geçerli bir tamsayı değil.")
 
     try:
-        BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS", "").split())
+        BL_CHATS = set(int(x) for x in os.environ.get("KARA_LİSTE_SOHBETLER", "").split())
     except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
+        raise Exception("Siyah listeli sohbetler listeniz geçerli tamsayılar içermiyor.")
 
     try:
         DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "7157587567").split())
+        DEV_USERS = set(int(x) for x in os.environ.get("DEVS", "7157587567").split())
     except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
+        raise Exception("Sudo veya geliştirici kullanıcılar listeniz geçerli tamsayılar içermiyor.")
 
     try:
         DEMONS = set(int(x) for x in os.environ.get("DEMONS", "").split())
     except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
+        raise Exception("Destek kullanıcılar listeniz geçerli tamsayılar içermiyor.")
 
     try:
         TIGERS = set(int(x) for x in os.environ.get("TIGERS", "").split())
     except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
+        raise Exception("Tiger kullanıcılar listeniz geçerli tamsayılar içermiyor.")
 
     try:
         WOLVES = set(int(x) for x in os.environ.get("WOLVES", "").split())
     except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
+        raise Exception("Beyaz listeli kullanıcılar listeniz geçerli tamsayılar içermiyor.")
 
 else:
     from JarvisRobo.config import Development as Config
@@ -112,33 +112,33 @@ else:
     try:
         OWNER_ID = int(Config.OWNER_ID)
     except ValueError:
-        raise Exception("Your OWNER_ID variable is not a valid integer.")
+        raise Exception("SAHİP_KİMLİĞİ değişkeniniz geçerli bir tamsayı değil.")
 
     try:
         BL_CHATS = set(int(x) for x in Config.BL_CHATS or [])
     except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
+        raise Exception("Siyah listeli sohbetler listeniz geçerli tamsayılar içermiyor.")
 
     try:
         DRAGONS = set(int(x) for x in Config.DRAGONS or [])
         DEV_USERS = set(int(x) for x in Config.DEV_USERS or [])
     except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
+        raise Exception("Sudo veya geliştirici kullanıcılar listeniz geçerli tamsayılar içermiyor.")
 
     try:
         DEMONS = set(int(x) for x in Config.DEMONS or [])
     except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
+        raise Exception("Destek kullanıcılar listeniz geçerli tamsayılar içermiyor.")
 
     try:
         TIGERS = set(int(x) for x in Config.TIGERS or [])
     except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
+        raise Exception("Tiger kullanıcılar listeniz geçerli tamsayılar içermiyor.")
 
     try:
         WOLVES = set(int(x) for x in Config.WOLVES or [])
     except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
+        raise Exception("Beyaz listeli kullanıcılar listeniz geçerli tamsayılar içermiyor.")
 
 
 DRAGONS.add(OWNER_ID)
@@ -151,7 +151,7 @@ pbot = Client("JarvisRobo", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN,in
 dispatcher = updater.dispatcher
 aiohttpsession = ClientSession()
 
-print("[INFO]: Getting Bot Info...")
+print("[BİLGİ]: Bot Bilgileri Alınıyor...")
 BOT_ID = dispatcher.bot.id
 BOT_NAME = dispatcher.bot.first_name
 BOT_USERNAME = dispatcher.bot.username
@@ -162,14 +162,15 @@ WOLVES = list(WOLVES)
 DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
-# Load at end to ensure all prev variables have been set
+# Tüm önceki değişkenlerin ayarlanmasını sağlamak için sona yükle
 from JarvisRobo.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
 )
 
-# make sure the regex handler can take extra kwargs
+# ekstra kwargs'ları alan regex handler'ı almak için emin olun
 tg.RegexHandler = CustomRegexHandler
 tg.CommandHandler = CustomCommandHandler
 tg.MessageHandler = CustomMessageHandler
+    
