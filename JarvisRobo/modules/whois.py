8fe71155f7306c804f11428ecd097bf6271332ff
Ali930Ali <alibeff48@gmail.com>
2024-06-06 14:37:03 +0400

    Update whois.py

--- JarvisRobo/modules/whois.py
+++ JarvisRobo/modules/whois.py
@@ -7,81 +7,82 @@ from pyrogram.types import Message, User
 from JarvisRobo import pbot
 
 
-def ReplyCheck(message: Message):
-    reply_id = None
+def CevapKontrol(message: Message):
+    cevap_id = None
 
     if message.reply_to_message:
-        reply_id = message.reply_to_message.message_id
+        cevap_id = message.reply_to_message.message_id
 
     elif not message.from_user.is_self:
-        reply_id = message.message_id
+        cevap_id = message.message_id
 
-    return reply_id
+    return cevap_id
 
 
-infotext = (
+bilgitext = (
     "[{full_name}](tg://user?id={user_id})\n\n"
-    " ➻ ᴜsᴇʀ ɪᴅ: `{user_id}`\n"
-    " ➻ ғɪʀsᴛ ɴᴀᴍᴇ: `{first_name}`\n"
-    " ➻ ʟᴀsᴛ ɴᴀᴍᴇ: `{last_name}`\n"
-    " ➻ ᴜsᴇʀɴᴀᴍᴇ: `@{username}`\n"
-    " ➻ ʟᴀsᴛ sᴇᴇɴ: `{last_online}`"
+    " ➻ ᴋᴜʟʟᴀɴɪᴄɪ ɪᴅ: `{user_id}`\n"
+    " ➻ ɪsɪᴍ: `{first_name}`\n"
+    " ➻ ᴠᴇ ᴅᴏɢʀᴜ ɪsɪᴍ: `{last_name}`\n"
+    " ➻ ᴋᴜʟʟᴀɴɪᴄɪ ᴀᴅɪ: `@{username}`\n"
+    " ➻ ᴠᴇ sᴏɴ ɢᴏʀᴜʟᴇɴ: `{last_online}`"
 )
 
 
-def LastOnline(user: User):
-    if user.is_bot:
+def SonGörülme(kullanıcı: User):
+    if kullanıcı.is_bot:
         return ""
-    elif user.status == "recently":
-        return "ʀᴇᴄᴇɴᴛʟʏ"
-    elif user.status == "within_week":
-        return "ᴡɪᴛʜɪɴ ᴛʜᴇ ʟᴀsᴛ ᴡᴇᴇᴋ"
-    elif user.status == "within_month":
-        return "ᴡɪᴛʜɪɴ ᴛʜᴇ ʟᴀsᴛ ᴍᴏɴᴛʜ"
-    elif user.status == "long_time_ago":
-        return "ᴀ ʟᴏɴɢ ᴛɪᴍᴇ ᴀɢᴏ :("
-    elif user.status == "online":
-        return "ᴄᴜʀʀᴇɴᴛʟʏ ᴏɴʟɪɴᴇ"
-    elif user.status == "offline":
-        return datetime.fromtimestamp(user.status.date).strftime(
+    elif kullanıcı.status == "recently":
+        return "Son günlerde"
+    elif kullanıcı.status == "within_week":
+        return "Son bir hafta içinde"
+    elif kullanıcı.status == "within_month":
+        return "Son bir ay içinde"
+    elif kullanıcı.status == "long_time_ago":
+        return "Uzun zaman önce :("
+    elif kullanıcı.status == "online":
+        return "Şu anda çevrimiçi"
+    elif kullanıcı.status == "offline":
+        return datetime.fromtimestamp(kullanıcı.status.date).strftime(
             "%a, %d %b %Y, %H:%M:%S"
         )
 
 
-def FullName(user: User):
-    return user.first_name + " " + user.last_name if user.last_name else user.first_name
+def Tamİsim(kullanıcı: User):
+    return kullanıcı.first_name + " " + kullanıcı.last_name if kullanıcı.last_name else kullanıcı.first_name
 
 
-@pbot.on_message(filters.command("whois"))
-async def whois(client, message):
+@pbot.on_message(filters.command("kim"))
+async def kim(client, message):
     cmd = message.command
     if not message.reply_to_message and len(cmd) == 1:
-        get_user = message.from_user.id
+        kullanıcı_al = message.from_user.id
     elif len(cmd) == 1:
-        get_user = message.reply_to_message.from_user.id
+        kullanıcı_al = message.reply_to_message.from_user.id
     elif len(cmd) > 1:
-        get_user = cmd[1]
+        kullanıcı_al = cmd[1]
         try:
-            get_user = int(cmd[1])
+            kullanıcı_al = int(cmd[1])
         except ValueError:
             pass
     try:
-        user = await client.get_users(get_user)
+        kullanıcı = await client.get_users(kullanıcı_al)
     except PeerIdInvalid:
-        await message.reply("I don't know that user.")
+        await message.reply("Bu kullanıcıyı tanımıyorum.")
         return
-    desc = await client.get_chat(get_user)
-    desc = desc.description
+    açıklama = await client.get_chat(kullanıcı_al)
+    açıklama = açıklama.description
     await message.reply_text(
-        infotext.format(
-            full_name=FullName(user),
-            user_id=user.id,
-            user_dc=user.dc_id,
-            first_name=user.first_name,
-            last_name=user.last_name if user.last_name else "",
-            username=user.username if user.username else "",
-            last_online=LastOnline(user),
-            bio=desc if desc else "`ᴇᴍᴩᴛʏ.`",
+        bilgitext.format(
+            full_name=Tamİsim(kullanıcı),
+            user_id=kullanıcı.id,
+            user_dc=kullanıcı.dc_id,
+            first_name=kullanıcı.first_name,
+            last_name=kullanıcı.last_name if kullanıcı.last_name else "",
+            username=kullanıcı.username if kullanıcı.username else "",
+            last_online=SonGörülme(kullanıcı),
+            bio=açıklama if açıklama else "`Boş.`",
         ),
         disable_web_page_preview=True,
     )
+    
