9d929c8902975d22e73bb386e8c29b98af62d49b
Ali930Ali <alibeff48@gmail.com>
2024-06-19 23:22:29 +0400

    Update carbon.py

--- JarvisRobo/modules/carbon.py
+++ JarvisRobo/modules/carbon.py
@@ -12,19 +12,19 @@ async def carbon_func(_, message):
         if message.reply_to_message.text:
             txt = message.reply_to_message.text
         else:
-            return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ.")
+            return await message.reply_text("bir mesaja cevap ver veya ver.")
     else:
         try:
             txt = message.text.split(None, 1)[1]
         except IndexError:
-            return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ.")
-    m = await message.reply_text("ɢᴇɴᴇʀᴀᴛɪɴɢ ᴄᴀʀʙᴏɴ...")
+            return await message.reply_text("bir mesaja cevap verin veya mesaj gönderin.")
+    m = await message.reply_text("karbon üretmek...")
     carbon = await make_carbon(txt)
-    await m.edit_text("ᴜᴩʟᴏᴀᴅɪɴɢ ɢᴇɴᴇʀᴀᴛᴇᴅ ᴄᴀʀʙᴏɴ...")
+    await m.edit_text("üretilen karbonun yüklenmesi...")
     await pbot.send_photo(
         message.chat.id,
         photo=carbon,
-        caption=f"» ʀᴇᴏ̨ᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}",
+        caption=f"» tarafından talep edildi : {message.from_user.mention}",
     )
     await m.delete()
     carbon.close()
@@ -33,8 +33,7 @@ __mod_name__ = "✨kᴀʀʙᴏɴ✨"
 
 __help__ = """
 
-ᴍᴀᴋᴇs ᴀ ᴄᴀʀʙᴏɴ ᴏғ ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ ᴀɴᴅ sᴇɴᴅ ɪᴛ ᴛᴏ ʏᴏᴜ.
-
-❍ /carbon *:* ᴍᴀᴋᴇs ᴄᴀʀʙᴏɴ ɪғ ʀᴇᴩʟɪᴇᴅ ᴛᴏ ᴀ ᴛᴇxᴛ
+Verilen metnin bir karbonunu yapar ve size gönderir.
+❍ /carbon *:* bir metne yanıt vermek karbon yapar
 
  """
